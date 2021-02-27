5a8f65b9a6b1f8b9cdba6f602878963d548512f6
EdwardBeshara711 <37589799+EdwardBeshara711@users.noreply.github.com>
2021-02-26 20:35:18 -1000

    working prototype with 3 drop down filters

--- carFinder/Form1.Designer.cs
+++ carFinder/Form1.Designer.cs
@@ -29,46 +29,81 @@ namespace carFinder
         /// </summary>
         private void InitializeComponent()
         {
+            this.cbDriveType = new System.Windows.Forms.ComboBox();
+            this.cbTransmission = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.cbMake = new System.Windows.Forms.ComboBox();
-            this.cbModel = new System.Windows.Forms.ComboBox();
-            this.cbYear = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.dgCarList = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dgCarList)).BeginInit();
             this.SuspendLayout();
+            // 
+            // cbDriveType
+            // 
+            this.cbDriveType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDriveType.FormattingEnabled = true;
+            this.cbDriveType.Location = new System.Drawing.Point(12, 25);
+            this.cbDriveType.Name = "cbDriveType";
+            this.cbDriveType.Size = new System.Drawing.Size(375, 21);
+            this.cbDriveType.TabIndex = 4;
+            this.cbDriveType.SelectedIndexChanged += new System.EventHandler(this.cbDriveType_SelectedIndexChanged);
+            // 
+            // cbTransmission
+            // 
+            this.cbTransmission.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTransmission.FormattingEnabled = true;
+            this.cbTransmission.Location = new System.Drawing.Point(12, 65);
+            this.cbTransmission.Name = "cbTransmission";
+            this.cbTransmission.Size = new System.Drawing.Size(375, 21);
+            this.cbTransmission.TabIndex = 5;
+            this.cbTransmission.SelectedIndexChanged += new System.EventHandler(this.cbTransmission_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Car Drive Train";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Transmission";
             // 
             // cbMake
             // 
             this.cbMake.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbMake.FormattingEnabled = true;
-            this.cbMake.Location = new System.Drawing.Point(12, 61);
+            this.cbMake.Location = new System.Drawing.Point(12, 105);
             this.cbMake.Name = "cbMake";
             this.cbMake.Size = new System.Drawing.Size(375, 21);
-            this.cbMake.TabIndex = 0;
+            this.cbMake.TabIndex = 8;
             this.cbMake.SelectedIndexChanged += new System.EventHandler(this.cbMake_SelectedIndexChanged);
             // 
-            // cbModel
+            // label3
             // 
-            this.cbModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbModel.FormattingEnabled = true;
-            this.cbModel.Location = new System.Drawing.Point(12, 88);
-            this.cbModel.Name = "cbModel";
-            this.cbModel.Size = new System.Drawing.Size(375, 21);
-            this.cbModel.TabIndex = 1;
-            // 
-            // cbYear
-            // 
-            this.cbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbYear.FormattingEnabled = true;
-            this.cbYear.Location = new System.Drawing.Point(12, 34);
-            this.cbYear.Name = "cbYear";
-            this.cbYear.Size = new System.Drawing.Size(375, 21);
-            this.cbYear.TabIndex = 2;
-            this.cbYear.SelectedIndexChanged += new System.EventHandler(this.cbYear_SelectedIndexChanged);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Make";
             // 
             // dgCarList
             // 
             this.dgCarList.AllowUserToAddRows = false;
             this.dgCarList.AllowUserToDeleteRows = false;
+            this.dgCarList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgCarList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dgCarList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgCarList.Location = new System.Drawing.Point(12, 269);
             this.dgCarList.Name = "dgCarList";
@@ -81,23 +116,29 @@ namespace carFinder
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1130, 669);
-            this.Controls.Add(this.dgCarList);
-            this.Controls.Add(this.cbYear);
-            this.Controls.Add(this.cbModel);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.cbMake);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbTransmission);
+            this.Controls.Add(this.cbDriveType);
+            this.Controls.Add(this.dgCarList);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Car Finder";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgCarList)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.ComboBox cbDriveType;
+        private System.Windows.Forms.ComboBox cbTransmission;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cbMake;
-        private System.Windows.Forms.ComboBox cbModel;
-        private System.Windows.Forms.ComboBox cbYear;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridView dgCarList;
     }
 }
--- carFinder/Form1.cs
+++ carFinder/Form1.cs
@@ -24,54 +24,122 @@ namespace carFinder
         private void Form1_Load(object sender, EventArgs e)
         {
 
-           //select * from ebLists
-            var carsYear = carDataEntities.ebLists
-                .Select(c => new { year = c.year })
+            var carDriveType = carDataEntities.ebLists
+                .Select(dt => new { drive = dt.drive })
                 .Distinct()
-                .OrderBy(c => c.year)
                 .ToList();
 
-            cbYear.DisplayMember = "year";
-            cbYear.DataSource = carsYear;
-                                  
+            cbDriveType.DisplayMember = "drive";
+            cbDriveType.ValueMember = "drive";
+            cbDriveType.DataSource = carDriveType;
+
+            
+        }
+        private void cbDriveType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedDriveType = cbDriveType.GetItemText(cbDriveType.SelectedValue);
+
+            //MessageBox.Show($"{selectedDriveType + selectedDriveType.GetType()}");
+            
+
+            var carTrans = carDataEntities.ebLists
+                .Where(ct => ct.drive == selectedDriveType)
+                .Select(ct => new { trany = ct.trany })
+                .Distinct()
+                .OrderBy(ct => ct.trany)
+                .ToList();
+
+            cbTransmission.DisplayMember = "trany";
+            cbTransmission.ValueMember = "id";
+            cbTransmission.DataSource = carTrans;
+
+
+        }
+
+        private void cbTransmission_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedDriveType = cbDriveType.GetItemText(cbDriveType.SelectedValue);
+            var selectedTransmission = cbTransmission.GetItemText(cbTransmission.SelectedValue);
+
+            var carMake = carDataEntities.ebLists
+                .Where(cm => cm.drive == selectedDriveType && cm.trany == selectedTransmission)
+                .Select(cm => new { make = cm.make })
+                .OrderBy(cm => cm.make)
+                .Distinct()
+                .ToList();
+
+            cbMake.DisplayMember = "make";
+            cbMake.ValueMember = "id";
+            cbMake.DataSource = carMake;
+
+
+
+
         }
 
         private void cbMake_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //MessageBox.Show("index changed");
-            string selectedMake = cbMake.GetItemText(cbMake.SelectedItem);
+            var selectedDriveType = cbDriveType.GetItemText(cbDriveType.SelectedValue);
+            var selectedTransmission = cbTransmission.GetItemText(cbTransmission.SelectedValue);
+            var selectedMake = cbMake.GetItemText(cbMake.SelectedValue);
 
-            //MessageBox.Show($"{selectedMake}");
+            var carList = carDataEntities.ebLists
+              .Where(cl => cl.drive == selectedDriveType)
+              .Where(cl => cl.trany == selectedTransmission) 
+              .Where(cl => cl.make == selectedMake)
+              .Distinct()
+              .ToList();
 
-            var carsModels = carDataEntities.ebLists
-                .Where(m => m.make == selectedMake)
-                .Select(m => new { model = m.model })
-                .Distinct()
-                .OrderBy(m => m.model)
-                .ToList();
-
-            cbModel.DisplayMember = "model";
-            cbModel.DataSource = carsModels;
-
+            dgCarList.DataSource = carList;
+            dgCarList.Columns["make"].DisplayIndex = 0;
+            dgCarList.Columns["model"].DisplayIndex = 1;
+            dgCarList.Columns["drive"].DisplayIndex = 2;
+            dgCarList.Columns["trany"].DisplayIndex = 3;
+            dgCarList.Columns["cylinders"].DisplayIndex = 4;
+            dgCarList.Columns["displ"].DisplayIndex = 5;
         }
 
-        private void cbYear_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var selectedYear = cbYear.GetItemText(cbYear.SelectedValue);
 
-            //MessageBox.Show($"{selectedYear.GetType()}");
+        //private void cbMake_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    ////MessageBox.Show("index changed");
+        //    //string selectedMake = cbMake.GetItemText(cbMake.SelectedItem);
 
-            var carMakes = carDataEntities.ebLists
-                .Where(y => y.year.ToString() == selectedYear)
-                .Select(c => new { make = c.make })
-                .Distinct()
-                .OrderBy(c => c.make)
-                .ToList();
+        //    ////MessageBox.Show($"{selectedMake}");
 
-            cbMake.DisplayMember = "make";
-            cbMake.DataSource = carMakes;
+        //    //var carsModels = carDataEntities.ebLists
+        //    //    .Where(m => m.make == selectedMake)
+        //    //    .Select(m => new { model = m.model })
+        //    //    .Distinct()
+        //    //    .OrderBy(m => m.model)
+        //    //    .ToList();
 
-            
-        }
+        //    //cbModel.DisplayMember = "model";
+        //    //cbModel.DataSource = carsModels;
+
+        //}
+
+        //private void cbYear_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    //var selectedYear = cbYear.GetItemText(cbYear.SelectedValue);
+
+        //    ////MessageBox.Show($"{selectedYear.GetType()}");
+
+        //    //var carMakes = carDataEntities.ebLists
+        //    //    .Where(y => y.year.ToString() == selectedYear)
+        //    //    .Select(c => new { make = c.make })
+        //    //    .Distinct()
+        //    //    .OrderBy(c => c.make)
+        //    //    .ToList();
+
+        //    //cbMake.DisplayMember = "make";
+        //    //cbMake.DataSource = carMakes;
+
+        //}
+
+        //private void cbTransmission_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+
+        //}
     }
 }
